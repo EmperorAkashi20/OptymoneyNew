2ff2e7ca531acf4b0bed45cb17d6c7c4ba98a49e
EmperorAkashi20 <rishabhsethiadsce@gmail.com>
2021-07-01 15:51:48 +0530

    Internet and sms permissions for android

---
+++ lib/Settings/Components/body.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class Body extends StatefulWidget {
+  const Body({Key? key}) : super(key: key);
+
+  @override
+  _BodyState createState() => _BodyState();
+}
+
+class _BodyState extends State<Body> {
+  @override
+  Widget build(BuildContext context) {
+    double windowHeight = MediaQuery.of(context).size.height;
+    double windowWidth = MediaQuery.of(context).size.width;
+    return Container();
+  }
+}
--- lib/Settings/Settings.dart
+++ lib/Settings/Settings.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:optymoney/Settings/Components/body.dart';
 
 class Settings extends StatelessWidget {
   static String routeName = '/Settings';
@@ -6,6 +7,8 @@ class Settings extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: Body(),
+    );
   }
 }
