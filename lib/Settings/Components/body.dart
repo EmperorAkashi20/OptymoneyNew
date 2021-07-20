b7a07bcc72d2494ca0001ec629b47a2bc053d9d1
EmperorAkashi20 <rishabhsethiadsce@gmail.com>
2021-07-20 13:30:45 +0530

    Profile section added

--- lib/Settings/Components/body.dart
+++ lib/Settings/Components/body.dart
@@ -12,6 +12,300 @@ class _BodyState extends State<Body> {
   Widget build(BuildContext context) {
     double windowHeight = MediaQuery.of(context).size.height;
     double windowWidth = MediaQuery.of(context).size.width;
-    return Container();
+    return Scaffold(
+      body: SingleChildScrollView(
+        child: Container(
+          child: Column(
+            children: [
+              Container(
+                height: windowHeight * 0.5,
+                width: windowWidth,
+                decoration: BoxDecoration(
+                  color: Colors.lightBlueAccent,
+                  borderRadius: BorderRadius.only(
+                    bottomRight: Radius.circular(30),
+                    bottomLeft: Radius.circular(30),
+                  ),
+                ),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    SizedBox(),
+                    Column(
+                      children: [
+                        CircleAvatar(
+                          radius: 50,
+                          child: Center(
+                            child: Text(
+                              'R',
+                              style: TextStyle(
+                                fontWeight: FontWeight.w700,
+                                color: Colors.white,
+                                fontSize: 40,
+                              ),
+                            ),
+                          ),
+                        ),
+                        SizedBox(
+                          height: 10,
+                        ),
+                        Text(
+                          'Rishabh Sethia',
+                          style: TextStyle(
+                              color: Colors.black54,
+                              fontSize: 25,
+                              fontWeight: FontWeight.bold),
+                        ),
+                        Text(
+                          'rishabhsethia@optymoney.com',
+                          style: TextStyle(
+                              color: Colors.black54,
+                              fontSize: 18,
+                              fontWeight: FontWeight.bold),
+                        ),
+                      ],
+                    ),
+                    Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Text(
+                          'Profile Completion Status',
+                          style: TextStyle(fontSize: 15),
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.symmetric(
+                              horizontal: 8.0, vertical: 8.0),
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              GestureDetector(
+                                onTap: () {
+                                  setState(() {
+                                    _showSnackBar('Basic Details: Complete');
+                                  });
+                                },
+                                child: CircleAvatar(
+                                  radius: 15,
+                                  child: Center(
+                                    child: Icon(
+                                      Icons.check,
+                                      size: 15,
+                                    ),
+                                  ),
+                                ),
+                              ),
+                              Container(
+                                width: windowWidth * 0.3,
+                                height: 4,
+                                color: Colors.blue.shade700,
+                              ),
+                              GestureDetector(
+                                onTap: () {
+                                  setState(() {
+                                    _showSnackBar('Profile Status: Complete');
+                                  });
+                                },
+                                child: CircleAvatar(
+                                  radius: 15,
+                                  child: Center(
+                                    child: Icon(
+                                      Icons.check,
+                                      size: 15,
+                                    ),
+                                  ),
+                                ),
+                              ),
+                              Container(
+                                width: windowWidth * 0.3,
+                                height: 4,
+                                color: Colors.blue.shade700,
+                              ),
+                              GestureDetector(
+                                onTap: () {
+                                  setState(() {
+                                    _showSnackBar('KYC Status: Complete');
+                                  });
+                                },
+                                child: CircleAvatar(
+                                  radius: 15,
+                                  child: Center(
+                                    child: Icon(
+                                      Icons.check,
+                                      size: 15,
+                                    ),
+                                  ),
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+              ),
+              SizedBox(
+                height: windowHeight * 0.01,
+              ),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 8.0),
+                child: ListTile(
+                  title: Text(
+                    'Orders',
+                    style: TextStyle(fontSize: 20),
+                  ),
+                  trailing: Icon(Icons.arrow_forward_rounded),
+                  subtitle: Text('Check your order status'),
+                  onTap: () {},
+                  tileColor: Colors.lightBlue.withOpacity(0.3),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(20),
+                  ),
+                ),
+              ),
+              SizedBox(
+                height: windowHeight * 0.01,
+              ),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 8.0),
+                child: ListTile(
+                  title: Text(
+                    'Profile Details',
+                    style: TextStyle(fontSize: 20),
+                  ),
+                  trailing: Icon(Icons.arrow_forward_rounded),
+                  subtitle: Text('Tap to view or edit your data'),
+                  onTap: () {},
+                  tileColor: Colors.lightBlue.withOpacity(0.3),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(20),
+                  ),
+                ),
+              ),
+              SizedBox(
+                height: windowHeight * 0.01,
+              ),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 8.0),
+                child: ListTile(
+                  title: Text(
+                    'Bank Accounts',
+                    style: TextStyle(fontSize: 20),
+                  ),
+                  trailing: Icon(Icons.arrow_forward_rounded),
+                  subtitle: Text('Tap to view or edit your bank details'),
+                  onTap: () {},
+                  tileColor: Colors.lightBlue.withOpacity(0.3),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(20),
+                  ),
+                ),
+              ),
+              SizedBox(
+                height: windowHeight * 0.01,
+              ),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 8.0),
+                child: ListTile(
+                  title: Text(
+                    'Transaction History',
+                    style: TextStyle(fontSize: 20),
+                  ),
+                  trailing: Icon(Icons.arrow_forward_rounded),
+                  subtitle: Text('Tap to view your previous transactions'),
+                  onTap: () {},
+                  tileColor: Colors.lightBlue.withOpacity(0.3),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(20),
+                  ),
+                ),
+              ),
+              SizedBox(
+                height: windowHeight * 0.01,
+              ),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 8.0),
+                child: ListTile(
+                  title: Text(
+                    'Cart',
+                    style: TextStyle(fontSize: 20),
+                  ),
+                  trailing: Icon(Icons.arrow_forward_rounded),
+                  subtitle: Text('Tap to view your cart and checkout'),
+                  onTap: () {},
+                  tileColor: Colors.lightBlue.withOpacity(0.3),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(20),
+                  ),
+                ),
+              ),
+              SizedBox(
+                height: windowHeight * 0.01,
+              ),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 8.0),
+                child: Align(
+                  alignment: Alignment.centerLeft,
+                  child: TextButton(
+                    onPressed: () {},
+                    child: Text('Knowledge & FAQ'),
+                  ),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 8.0),
+                child: Align(
+                  alignment: Alignment.centerLeft,
+                  child: TextButton(
+                    onPressed: () {},
+                    child: Text('Contact an Expert'),
+                  ),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 8.0),
+                child: Align(
+                  alignment: Alignment.centerLeft,
+                  child: TextButton(
+                    onPressed: () {},
+                    child: Text('Help Center'),
+                  ),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 8.0),
+                child: Align(
+                  alignment: Alignment.centerLeft,
+                  child: TextButton(
+                    onPressed: () {},
+                    child: Text('Privacy Policy'),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  void _showSnackBar(String text) {
+    final snackBar = SnackBar(
+      duration: const Duration(seconds: 3),
+      content: Container(
+        height: 40.0,
+        color: Colors.transparent,
+        child: Center(
+          child: Text(
+            text,
+            style: const TextStyle(fontSize: 15.0, color: Colors.black),
+          ),
+        ),
+      ),
+      backgroundColor: Colors.white,
+    );
+    ScaffoldMessenger.of(context)
+      ..hideCurrentSnackBar()
+      ..showSnackBar(snackBar);
   }
 }
